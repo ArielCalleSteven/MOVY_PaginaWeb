77597ac86030c9c80c4795a97ebdf5310b835549
ArielCalleSteven <a.calleduma123@gmail.com>
2023-01-09 02:43:20 -0500

    hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PaginaComponent } from './pagina/pagina.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: LoginComponent
+
+  },
+  {
+    path:'pag2', component: PaginaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
